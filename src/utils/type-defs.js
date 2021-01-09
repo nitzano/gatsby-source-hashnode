8f4e4b3259899ae3fe315459b9d53ff85a252a63
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-01-09 12:42:51 +0200

    added description

--- src/utils/type-defs.js
+++ src/utils/type-defs.js
@@ -7,15 +7,19 @@ export const typeDefs = gql`
     contentMarkdown: String
     coverImage: String
     cuid: String
+    # publication date
     dateAdded: String
     slug: String
     title: String
     readingTime: ReadingTime
   }
 
+  # Reading time helper
   type ReadingTime {
+    # full time description
     text: String
     minutes: Int
+    # time (in milliseconds)
     time: Int
     words: Int
   }
