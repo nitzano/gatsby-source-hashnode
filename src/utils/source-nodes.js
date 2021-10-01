15a30c0c8ee7128eb72c0e82f56168897055c5ab
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-10-01 14:27:21 +0300

    with try catch

--- src/utils/source-nodes.js
+++ src/utils/source-nodes.js
@@ -10,53 +10,58 @@ export async function sourceNodes(
   { actions, createNodeId, createContentDigest, getCache },
   { username }
 ) {
-  logger("sourcing-nodes");
+  try {
+    logger("sourcing-nodes");
 
-  const { createNode } = actions;
-  // check username exists
-  if (!username) {
-    throw new Error("no username supplied");
-  }
+    const { createNode } = actions;
+    // check username exists
+    if (!username) {
+      throw new Error("no username supplied");
+    }
 
-  // create hash node user
-  const userDetails = await getUserDetails(username);
+    // create hash node user
+    const userDetails = await getUserDetails(username);
 
-  const hashNodeUser = {
-    ...userDetails,
-    id: createNodeId(userDetails._id),
-    parent: null,
-    children: [],
-    internal: {
-      type: "HashNodeUser",
-      description: "details about the user",
-      contentDigest: createContentDigest(userDetails._id),
-    },
-  };
-
-  createNode(hashNodeUser);
-
-  // get posts
-  const posts = await getUserPosts(username);
-
-  // process posts
-  posts.map((post) => {
-    const { _id, contentMarkdown = "" } = post;
-
-    const nodeSchema = {
-      ...post,
-      readingTime: readingTime(contentMarkdown),
-      id: createNodeId(_id),
+    const hashNodeUser = {
+      ...userDetails,
+      id: createNodeId(userDetails._id),
       parent: null,
       children: [],
       internal: {
-        type: HASHNODE_NODE_TYPE,
-        mediaType: `text/markdown`,
-        content: contentMarkdown,
-        contentDigest: createContentDigest(post),
+        type: "HashNodeUser",
+        description: "details about the user",
+        contentDigest: createContentDigest(userDetails._id),
       },
     };
 
-    // create the node
-    createNode(nodeSchema);
-  });
+    createNode(hashNodeUser);
+
+    // get posts
+    const posts = await getUserPosts(username);
+
+    // process posts
+    posts.map((post) => {
+      const { _id, contentMarkdown = "" } = post;
+
+      const nodeSchema = {
+        ...post,
+        readingTime: readingTime(contentMarkdown),
+        id: createNodeId(_id),
+        parent: null,
+        children: [],
+        internal: {
+          type: HASHNODE_NODE_TYPE,
+          mediaType: `text/markdown`,
+          content: contentMarkdown,
+          contentDigest: createContentDigest(post),
+        },
+      };
+
+      // create the node
+      createNode(nodeSchema);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
