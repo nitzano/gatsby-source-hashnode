5817fb561f73e50273f67b2fab5053aaf617bc3d
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-10-01 14:27:46 +0300

    with source

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -2,6 +2,6 @@ import { createSchemaCustomization } from "./utils/create-schema-customization";
 import { onCreateNode } from "./utils/on-create-node";
 import { sourceNodes } from "./utils/source-nodes";
 
-exports.sourceNodes = sourceNodes;
 exports.onCreateNode = onCreateNode;
+exports.sourceNodes = sourceNodes;
 exports.createSchemaCustomization = createSchemaCustomization;
